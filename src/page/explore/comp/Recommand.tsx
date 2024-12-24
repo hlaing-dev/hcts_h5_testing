b972aa8645e32171fa5ccc22a3bed7f28bb2ace6
hlaing-dev <hlaingmin1235145@gmail.com>
2024-12-25 00:33:05 +0700

    update

--- src/page/explore/comp/Recommand.tsx
+++ src/page/explore/comp/Recommand.tsx
@@ -17,16 +17,15 @@ const Recommand: React.FC<RecommandProps> = ({ title }) => {
     if (data?.data) {
       setList(data?.data.list);
     }
-  }, [data, list]);
+  }, [data, list, isLoading]);
   const navigate = useNavigate();
   const [refresh, setRefresh] = useState<boolean>(false);
-  const refreshCard = () => {
-    setRefresh(true);
-    setTimeout(() => {
-      setRefresh(false);
-    }, 300);
-    clearTimeout;
+  const refreshCard = async () => {
+    setRefresh(true); // Show loading animation
+    await refetch(); // Refetch data
+    setRefresh(false); // Hide loading animation after refetch
   };
+
   return (
     <div className=" pb-[20px] pt-[10px]">
       {/* header */}
@@ -41,64 +40,52 @@ const Recommand: React.FC<RecommandProps> = ({ title }) => {
       </div>
       {/* content */}
       <div className=" py-[12px] grid grid-cols-2 gap-[18px]">
-        {refresh ? (
-          <>
-            <div className=" grid grid-cols-2 gap-[20px] justify-center">
-              <div className="flex items-center gap-[20px] space-x-4">
-                <div className="space-y-2">
-                  <div className=" w-[335px] h-[150px] animate-pulse bg-slate-700"></div>
-                </div>
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            {isLoading ? (
-              <>
-                <div className="w-[175px] h-[120px] bg-white/20"></div>
-                <div className="w-[175px] h-[120px] bg-white/20"></div>
-                <div className="w-[175px] h-[120px] bg-white/20"></div>
-                <div className="w-[175px] h-[120px] bg-white/20"></div>
-              </>
-            ) : (
-              <>
-                {list?.slice(0, 4).map((card: any) => (
-                  <div key={card.post_id} className="w-[175px">
-                    <div className=" relative  chinese_photo">
-                      <img
-                        className=" w-[175px] h-[100px] rounded-[8px] object-cover"
-                        src={card.preview_image}
-                        alt=""
-                      />
-                      <span className=" text-white text-[11px] absolute bottom-2 left-2">
-                        29.3k views
-                      </span>
-                      <span className=" text-white text-[11px] absolute bottom-2 right-2">
-                        00:23:32
-                      </span>
-                    </div>
-                    <h1 className="text-white text-[14px] font-[500] leading-[20px] py-[4px]">
-                      {card.title.length > 20
-                        ? `${card.title.slice(0, 20)}...`
-                        : card.title}
-                    </h1>
-                    {/* uploader */}
-                    <div className=" flex justify-cente py-[4px] items-center gap-[8px]">
-                      <img
-                        className=" w-[26px] h-[26px] rounded-full"
-                        src={card.user.avatar ? card.user.avatar : avatar}
-                        alt=""
-                      />
-                      <h1 className=" text-white text-[12px] font-[400] leading-[20px]">
-                        {card.user.name}
-                      </h1>
-                    </div>
+        <>
+          {isLoading || refresh ? (
+            <>
+              <div className="w-[175px] h-[140px] bg-white/20"></div>
+              <div className="w-[175px] h-[140px] bg-white/20"></div>
+              <div className="w-[175px] h-[140px] bg-white/20"></div>
+              <div className="w-[175px] h-[140px] bg-white/20"></div>
+            </>
+          ) : (
+            <>
+              {list?.slice(0, 4).map((card: any) => (
+                <div key={card.post_id} className="w-[175px">
+                  <div className=" relative  chinese_photo">
+                    <img
+                      className=" w-[175px] h-[100px] rounded-[8px] object-cover"
+                      src={card.preview_image}
+                      alt=""
+                    />
+                    <span className=" text-white text-[11px] absolute bottom-2 left-2">
+                      29.3k views
+                    </span>
+                    <span className=" text-white text-[11px] absolute bottom-2 right-2">
+                      00:23:32
+                    </span>
                   </div>
-                ))}
-              </>
-            )}
-          </>
-        )}
+                  <h1 className="text-white text-[14px] font-[500] leading-[20px] py-[4px]">
+                    {card.title.length > 20
+                      ? `${card.title.slice(0, 20)}...`
+                      : card.title}
+                  </h1>
+                  {/* uploader */}
+                  <div className=" flex justify-cente py-[4px] items-center gap-[8px]">
+                    <img
+                      className=" w-[26px] h-[26px] rounded-full"
+                      src={card.user.avatar ? card.user.avatar : avatar}
+                      alt=""
+                    />
+                    <h1 className=" text-white text-[12px] font-[400] leading-[20px]">
+                      {card.user.name}
+                    </h1>
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
+        </>
       </div>
       {/* buttons */}
       <div className="flex justify-center gap-[20px] px-[10px]">

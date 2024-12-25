945a670742201189c4f051851b52d2dbd2a312d1
hlaing-dev <hlaingmin1235145@gmail.com>
2024-12-25 10:27:47 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'build', // Change the output directory to 'build'
-  },
-  base: "./",
 });
